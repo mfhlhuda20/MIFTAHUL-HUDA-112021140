22ab6077339b4a141f75a6bf821609ead83c63b1
mfhlhuda20 <148615673+mfhlhuda20@users.noreply.github.com>
2023-11-02 02:38:37 +0700

    Add files via upload

---
+++ TUGAS MATLAB MIFTAHUL HUDA _112021140.m
@@ -0,0 +1,87 @@
+%MIFTAHUL HUDA
+%112021140
+%SOAL 1 : MATRIKS
+A = [1 2 3 ; 2 1 1 ; 3 2 1]
+B = [4 4 5 ; 6 2 2 ; 3 5 5]
+%Dengan menggunakan matlab tentukan:
+%soal a.
+C =det(A)
+C =det(B)
+%soal B.
+C = size (A)
+C = size (B)
+%soalc.
+C = trace (A)
+C = trace (B)
+%soal d.
+C = norm (A)
+C = norm (B)
+% soal e.
+C = A + B
+%soal F.
+C = A - B
+%soal G.
+C = A * B
+%soal H.
+C = A.* B
+%soal I.
+C = A^2
+%soal J.
+C = A.^2
+% soal k.
+C = A'
+C = B'
+% soal L.
+C = A./ B
+%soal M.
+C = A.\ B
+%soal N.
+C = A/B
+% o. invers dari matrik A dan B
+C = inv (A)
+C = inv (B)
+% Soal P.
+C = null (A)
+% soal Q.
+C = orth (A)
+%oal R.
+C = rref (A)
+% s. nilai eigen dari matrik A dan B
+C = eig (A)
+C = eig (B)
+% t, nilai singular matrik A dan B
+C = svd (A)
+C = svd (B)
+% u. matrik segitiga atas dari matrik A dan B
+C = triu (A)
+C = triu (B)
+% V. matrik segitiga bawah dari matrik A dan B
+C = tril (A)
+C = tril (B)
+% w. tentukan nilai maksimum daei elemen elemen matrik A dan B
+C = max (A)
+C = max (B)
+% x. tentukan nilai daari minimum matik A dan B
+C = min (A)
+C = min (B)
+% y. tentukan jumlah kolom elemen elemen matrik A dan B
+C = sum (A,1)
+C = sum (A,2)
+C = sum (B,1)
+C = sum (B,2)
+%z, tentukan diagonaldari matrik A dan B
+C = diag (A)
+C = diag (B)
+% SSOAL 2:
+% a, matrik identitas dimensi 5x5
+A=eye(5)
+% b, matrik dengan dimensi 3x3 bernilai nol 
+A=zeros(3,3)
+% c, matrik dengan dimensi 4x4 bernilai 1
+A=ones (4,4)
+% d. matrik dengan dimensi 3x4 terdistribusi abtra 0 dan 1 (default n=m)
+A=rand (3,4)
+% e. matrik dengan dimensi 3x4 terdistribusi normal 
+A=randn(3,4)
+
+
